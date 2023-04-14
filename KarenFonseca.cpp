76612913cfd2ad61630fcca45f2c0420708af104
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2023-04-14 10:35:08 -0600

    Datos personales - Analista de sistemas (archivo guardado)

---
+++ KarenFonseca.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void datosPersonales(){
+    cout << "Mi nombre es Karen Fonseca. Soy estudiante de 1er anio de Ing. en Sistemas en la UAM." << endl;
+    cout << "Usuario de Github: littlesaturnalien." << endl;
+    cout << "Me corresponde el rol de analista de sistemas." << endl;
+}
