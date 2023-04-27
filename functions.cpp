f67b1a0d8b602d59ea5bd8808ea93303c63c2afa
vexfairyprincess <samora@uamv.edu.ni>
2023-04-26 22:11:18 -0600

    update

--- functions.cpp
+++ functions.cpp
@@ -8,14 +8,11 @@ using namespace std;
 studentsUAM students[MAX];
 int pos = 0;
 //Funcion de informacion estudiantil
-void add(int cif, char nam[], char lnam[], char fac[], char maj[], char em[] );
-//Funcion de estado de entrega de carnet
-void status(bool delivered = true, bool notDelivered = false);
+void add(int cif, char nam[], char fac[], char maj[], char em[], bool delivered = true, bool notDelivered = false );
 
 void add(int cif, char nam[], char lnam[], char fac[], char maj[], char em[] )
 {
     students[pos].cif = cif;
-    strcpy(students[pos].lastname, lnam);
     strcpy(students[pos].name, nam);
     strcpy(students[pos].major, maj);
     strcpy(students[pos].faculty, fac);
--- struct.h
+++ struct.h
@@ -3,14 +3,10 @@ typedef struct
 {
     int cif;
     char name[40];
-    char lastname[40];
     char major[16];
     char faculty[6];
     char email[40];
-}studentsUAM;
-
-typedef struct 
-{
     bool delivered;
     bool notDelivered;
-}status;
+}studentsUAM;
+
