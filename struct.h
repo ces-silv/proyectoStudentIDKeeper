aa5d2d657d1afbf2b1c2ebb460c6f9fb7acd9841
vexfairyprincess <samora@uamv.edu.ni>
2023-04-26 22:55:47 -0600

    update

--- struct.h
+++ struct.h
@@ -2,14 +2,15 @@
 typedef struct{
     int cif;
     char name[40];
-    char faculty[6];
-    char major[16];
-    char email[40];
+    char faculty[1000];
+    char major[1000];
+    char email[10000];
     bool deliveredOrNot;
 }studentsUAM;
 
 //Declaracion e inicializacion
 studentsUAM data[] = {
-
+    {18010053, "SILVIO ALEJANDRO MORA MENDOZA", "FACULTAD DE INGENIERIA Y ARQUITECTURA",
+    "INGENIERIA EN SISTEMAS DE INFORMACION", "samora@uamv.edu.ni", true},
 
 };
