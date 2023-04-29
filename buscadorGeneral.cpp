eaabbfc617f16462bf7695e7fbd769c862067243
ailanzas <ailanzasr@uamv.edu.ni>
2023-04-28 21:11:33 -0600

    AAH

--- buscadorGeneral.cpp
+++ buscadorGeneral.cpp
@@ -79,5 +79,369 @@ void buscadorNombre()
             cout << "No fue encontrado en nuesta base de datos" << endl;
             break;
         }
-    } 
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("CESAR AUGUSTO SILVA HERNANDEZ") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("KAREN MARIZA ROSA YOLANDA FONSECA VEGA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("ADRIANA ISABEL LANZAS RIVAS") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("DIEGO ROBERTO CORRALES ESPINOZA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("ADRIANO SAMUEL MEDINA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("ELISA MARGARITA CHAMORRO SANCHEZ") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+     for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("ALVIN JAVIER RODRIGUEZ VALDIVIA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+     for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("VICTORIA NAVARRETE PINEDA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("GIULIANNA ISABELLA HERNANDEZ RUIZ") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("MARIA IGNACIA MANZANARES LACAYO") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+     for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("MARCELA GABRIELA SALINAS MENDIETA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("AMY SILOE LOPEZ ESCOBAR") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("ALEXANDRA XIOMARA MEJIA WILFORD") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+     for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("CARLOS FERNANDO PEREZ ROMERO") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("CARMEN ANDREA GOMEZ GUADAMUZ") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("MATTHEW JAVIER CARRILLO ARANGO") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+     for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("ALEXANDER SAMUEL MORENO ENRIQUEZ") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+    for(int i = 0; i < 19; i++) {
+    string name = database[i].name;
+    if (name.compare("SANTIAGO ENRIQUE BALDOVINOS ZELAYA") == 0) {
+            cout << "CIF: " << database[i].cif << endl;
+            cout << "Nombre: " << database[i].name << endl;
+            cout << "Facultad: " << database[i].faculty << endl;
+            cout << "Carrera: " << database[i].major << endl;
+            cout << "email: " << database[i].email << endl;
+            if (database[i].deliveredOrNot == false){ 
+                cout << "Status: " << "No Entregado" << endl << endl;}
+            else{
+                cout << "Status: " << "Entregado" << endl << endl;}
+            cout << endl;
+        }
+        else{
+            system("clear");
+            cout << "No fue encontrado en nuesta base de datos" << endl;
+            break;
+        }
+    }
+
+
+
+
 }
