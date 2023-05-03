550875742020a411976b00de99ed129a0b3abb39
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2023-05-03 15:26:30 -0600

    Se agregó la función para regresar al menú principal

--- mainSearcher.cpp
+++ mainSearcher.cpp
@@ -11,6 +11,7 @@ void majorSearcher();
 void deliveredSearcher();
 void notDeliveredSearcher();
 void changeStatus();
+void returnToMenu();
 
 void menuMain(){
     int selectMenu;
@@ -22,7 +23,7 @@ void menuMain(){
     cout << "4. CARRERA" << endl;
     cout << "5. ENTREGADO" << endl;
     cout << "6. NO ENTREGADO" << endl;
-    cout << "7. Cambiar estado de entrega" << endl;
+    cout << "7. CAMBIAR ESTADO DE ENTREGA" << endl;
     cout << endl;
     cout << "---> ";
     cin >> selectMenu;
@@ -31,28 +32,57 @@ void menuMain(){
     caracteres que se ingresan mediante el teclado, en este caso el cin >> filtro .*/
     
     switch(selectMenu){
-        case 1: cifSearcher();
+        case 1: cifSearcher(); returnToMenu();
             break;
 
-        case 2: nameSearcher();
+        case 2: nameSearcher(); returnToMenu();
             break;
  
-        case 3: facultySearcher();
+        case 3: facultySearcher(); returnToMenu();
             break;
 
-        case 4: majorSearcher();
+        case 4: majorSearcher(); returnToMenu();
             break;
                 
-        case 5: deliveredSearcher();
+        case 5: deliveredSearcher(); returnToMenu();
             break;
         
-        case 6: notDeliveredSearcher();
+        case 6: notDeliveredSearcher(); returnToMenu();
             break;
 
-        case 7: changeStatus();
+        case 7: changeStatus(); returnToMenu();
         break;
 
         default: cout << "SALIO CON EXITO DE LOS FILTROS.";
+            break;
+    }
+}
+
+void returnToMenu(){
+    int options;
+    cout << endl;
+    cout << "DESEA REGRESAR AL MENU PRINCIPAL?" << endl;
+    cout << "1. SI" << endl;
+    cout << "2. NO" << endl;
+    cout << endl;
+    cout << "---> "; 
+    cin >> options;
+
+    switch (options){
+        case 1: 
+            system("cls");
+            menuMain();
+            break;
+
+        case 2: 
+            cout << endl;
+            cout<<"Ha cerrado sesion." << endl;
+            break;
+        
+        default: 
+            system("cls");
+            cout<<"Ingreso un numero invalido. Vuelva a intentarlo." << endl;
+            returnToMenu();
             break;
     }
 }
